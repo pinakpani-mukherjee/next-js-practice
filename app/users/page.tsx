8c84152e28b06aa00fabec0931b3b6135a9a9cd1
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2023-11-28 08:56:31 +0900

    init

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserTable from './UserTable'
+
+const UsersPage = () => {
+    return (
+        <>
+            <h1>Users</h1>
+            <UserTable />
+        </>
+    )
+}
+
+export default UsersPage
